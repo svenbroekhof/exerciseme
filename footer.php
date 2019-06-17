6f19e11ea147828290b2035dd5b0a492a07f0476
Tijn Smit <ionic.320625@gmail.com>
2019-06-17 08:58:59 +0200

    fit_en_footer

--- footer.php
+++ footer.php
@@ -1,3 +1,19 @@
 <footer>
-<h1>Test</h1>
+    <div id="footer_boven">
+        <div class="container">
+            <b>Exercise</b>Me
+        </div>
+    </div>
+    <div class="container">
+        <a style="float: right;">© 2019 ExerciseMe</a>
+        <?php 
+            if ( isset($_SESSION["id"])){
+              echo '
+                  <a id="footer" href="index.php?content=logout">Log Uit</a>';
+            } else{
+              echo '
+                  <a id="footer" href="index.php?content=aanmelden">Aanmelden</a>';
+            }
+          ?>
+    </div>
 </footer>
---
+++ trainingen.php
@@ -0,0 +1,286 @@
+<div class="row">
+    <div class="col-sm">
+        <div class="card">
+            <div id="licht" class="card-header text-center" data-toggle="collapse" href="#plank" aria-expanded="false" aria-controls="collapseExample">
+            beginner
+            </div>
+            <div>
+                <img id="img_card" src="./img/Plank.jpg" alt="push up" data-toggle="collapse" href="#plank" aria-expanded="false" aria-controls="collapseExample"/>
+                <div class="text-center" data-toggle="collapse" href="#plank" aria-expanded="false" aria-controls="collapseExample" style="height:35px;">
+                    <h5>Plank</h5><br>
+                </div>
+                <div>
+                    <div class="collapse container" id="plank">
+                        <br>
+                        -Pak een fitnessmat en ga met je buik richting de mat liggen.<br>
+                        -De armen zijn gebogen en plaats de onderarmen op de grond en de elleboog zit precies onder de schouder.<br>
+                        -Het gewicht van je onderlijf wordt gelift op de tenen.<br>
+                        -Houdt hoofd, nek, rug en heupen mooi horizontaal op één lijn.<br>
+                        -Trek de navel in richting je wervelkolom en span je buikspieren aan.<br>
+                        -Blijf goed ademhalen tijdens deze oefening.<br>
+                        -En probeer deze oefening op te bouwen totdat je hem een minuut kunt volhouden.<br>
+                        <br /><br />
+                        <div class="text-center">
+                            <a class="btn btn-primary btn-block" href="index.php?content=scripts/pushup_script" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                voltooid
+                            </a><div style="height:8px;"></div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+    </div>
+    <div class="col-sm">
+        <div class="card">
+            <div id="licht" class="card-header text-center" data-toggle="collapse" href="#ReverseCrunch" aria-expanded="false" aria-controls="collapseExample">
+            gevorderd
+            </div>
+            <div>
+                <img id="img_card" src="./img/Reverse-Crunch.jpg" alt="ReverseCrunch" data-toggle="collapse" href="#ReverseCrunch" aria-expanded="false" aria-controls="collapseExample"/>
+                <div class="text-center" data-toggle="collapse" href="#ReverseCrunch" aria-expanded="false" aria-controls="collapseExample" style="height:35px;">
+                    <h5>Reverse Crunch</h5><br>
+                </div>
+                <div>
+                    <div class="collapse container" id="ReverseCrunch">
+                        <br>
+                        -Ga liggen op een fitness mat en breng de benen omhoog met de knieën gebogen (90 graden).<br>
+                        -Plaats je handen naast je lichaam (voorkeur met de handpalmen naar boven).<br>
+                        -Lift je heup op van de vloer en beweeg de knieën iets richting je borst.<br>
+                        -Laat de heupen gecontroleerd weer zakken en herhaal voor het gewenste aantal repetities.<br>
+                        <br /><br />
+                        <div class="text-center">
+                            <a class="btn btn-primary btn-block" href="index.php?content=scripts/pushup_script" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                voltooid
+                            </a><div style="height:8px;"></div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+    </div>
+    <div class="col-sm">
+    <div class="card">
+            <div id="licht" class="card-header text-center" data-toggle="collapse" href="#PushUp" aria-expanded="false" aria-controls="collapseExample">
+            beginner
+            </div>
+            <div>
+                <img id="img_card" src="./img/pushup.jpg" alt="PushUp" data-toggle="collapse" href="#PushUp" aria-expanded="false" aria-controls="collapseExample"/>
+                <div class="text-center" data-toggle="collapse" href="#PushUp" aria-expanded="false" aria-controls="collapseExample" style="height:35px;">
+                    <h5>Push Up</h5><br>
+                </div>
+                <div>
+                    <div class="collapse container" id="PushUp">
+                        <br>
+                        -Ga op je knieen en ga vervolgens op je handen liggen, welke je op schouderbreedte van elkaar afzet.<br />
+                        -Strek vervolgens je knieen zodat je op je tenen komt te staan.<br />
+                        -Als het goed is, is je lichaam een rechte lijn.<br />
+                        -Dit is de starthouding van de push ups.<br />
+                        -Laat je langzaam door je armen zakken naar beneden, totdat je net boven de vloer komt. Raak de grond niet aan.<br />
+                        -Let op dat je alleen je armen beweegt, de rest van je lichaam blijft stil<br />
+                        -Druk je vervolgens weer op totdat je in de starthouding komt.<br />
+                        -Herhaal voor het gewenste aantal reps<br />
+                        <br /><br />
+                        <div class="text-center">
+                            <a class="btn btn-primary btn-block" href="index.php?content=scripts/pushup_script" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                voltooid
+                            </a><div style="height:8px;"></div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm">
+        <div class="card">
+            <div id="licht" class="card-header text-center" data-toggle="collapse" href="#SitUp" aria-expanded="false" aria-controls="collapseExample">
+            beginner
+            </div>
+            <div>
+                <img id="img_card" src="./img/Sit-Up-arms-crossed.jpg" alt="SitUp" data-toggle="collapse" href="#SitUp" aria-expanded="false" aria-controls="collapseExample"/>
+                <div class="text-center" data-toggle="collapse" href="#SitUp" aria-expanded="false" aria-controls="collapseExample" style="height:35px;">
+                    <h5>Sit Up</h5><br>
+                </div>
+                <div>
+                    <div class="collapse container" id="SitUp">
+                        <br>
+                        -Kies een platte vloer uit. Leg een fitness matje op de vloer.<br />
+                        -Klem je voeten vast onder een bankje<br />
+                        -Ga met je rug plat op de grond liggen<br />
+                        -Dit is de beginpositie van de oefening<br />
+                        -Houd niet je nek vast, maar houd je handen naast je hoofd en kom omhoog<br />
+                        -Kom zover omhoog tot je ellebogen je knieen raken<br />
+                        -Laat je langzaam weer zakken, zonder dat je rug de vloer raakt<br />
+                        -Herhaal voor het gewenst aantal repst kunt volhouden.<br>
+                        <br /><br />
+                        <div class="text-center">
+                            <a class="btn btn-primary btn-block" href="index.php?content=scripts/pushup_script" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                voltooid
+                            </a><div style="height:8px;"></div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+    </div>
+    <div class="col-sm">
+        <div class="card">
+            <div id="licht" class="card-header text-center" data-toggle="collapse" href="#AirSquat" aria-expanded="false" aria-controls="collapseExample">
+                beginner
+            </div>
+            <div>
+                <img id="img_card" src="./img/Air-Squat.jpg" alt="AirSquat" data-toggle="collapse" href="#AirSquat" aria-expanded="false" aria-controls="collapseExample"/>
+                <div class="text-center" data-toggle="collapse" href="#AirSquat" aria-expanded="false" aria-controls="collapseExample" style="height:35px;">
+                    <h5>Air Squat</h5><br>
+                </div>
+                <div>
+                    <div class="collapse container" id="AirSquat">
+                        <br>
+                        -Ga staan met je voeten iets wijder staan dan schouderbreedte<br />
+                        -Houd je borst recht omhoog en kijk voor je uit<br />
+                        -Dit is de beginpositie van de oefening<br />
+                        -Laat je langzaam door je knieen zakken, alsof je gaat zitten op een kruk, bal of stoel<br />
+                        -Strek je armen voor je uit wanneer je de neergaande beweging maakt<br />
+                        -Leg bij het zakken je lichaamsgewicht op je hielen<br />
+                        -Blijf voor je uit kijken terwijl je naar beneden zakt<br />
+                        -Kom parallel met de grond en druk vervolgens met je hielen jezelf weer omhoog<br />
+                        -Breng je armen weer naar beneden bij het omhoog komen<br />
+                        -Zorg ervoor dat je knieen niet op slot gaan wanneer je bovenin komt<br />
+                        -Herhaal voor het gewenste aantal reps<br />
+                        <br /><br />
+                        <div class="text-center">
+                            <a class="btn btn-primary btn-block" href="index.php?content=scripts/pushup_script" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                voltooid
+                            </a><div style="height:8px;"></div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+    </div>
+    <div class="col-sm">
+    <div class="card">
+            <div id="licht" class="card-header text-center" data-toggle="collapse" href="#SidePlank" aria-expanded="false" aria-controls="collapseExample">
+                gevorderd
+            </div>
+            <div>
+                <img id="img_card" src="./img/Side-Plank.jpg" alt="SidePlank" data-toggle="collapse" href="#SidePlank" aria-expanded="false" aria-controls="collapseExample"/>
+                <div class="text-center" data-toggle="collapse" href="#SidePlank" aria-expanded="false" aria-controls="collapseExample" style="height:35px;">
+                    <h5>Side Plank</h5><br>
+                </div>
+                <div>
+                    <div class="collapse container" id="SidePlank">
+                        <br>
+                        -Pak een fitnessmat en kantel jezelf op je zij.<br />
+                        -De onderarm wordt op de mat geplaatst met je elleboog direct onder je schouder en de elleboog is in een rechte lijn met de pols.<br />
+                        -Strek je lichaam lang uit en zorg dat de enkels, knieën, heupen, schouders, nek en hoofd op een rechte lijn met elkaar verbonden zijn.<br />
+                        -Breng de heup omhoog zodat het lichaam in een rechte lijn is van je enkels tot aan je hoofd.<br />
+                        -Wanneer je schoenen aan hebt kun je de voeten op elkaar leggen.<br />
+                        -Probeer de navel naar binnen te trekken richting de wervelkolom.<br />
+                        -Blijf goed ademhalen en let op je houding.<br />
+                        -Probeer deze oefening op te bouwen totdat je hem minimaal 30 seconden kunt vasthouden.<br />
+                        <br /><br />
+                        <div class="text-center">
+                            <a class="btn btn-primary btn-block" href="index.php?content=scripts/pushup_script" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                voltooid
+                            </a><div style="height:8px;"></div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm">
+        <div class="card">
+            <div id="licht" class="card-header text-center" data-toggle="collapse" href="#AbdominalAirBike" aria-expanded="false" aria-controls="collapseExample">
+                ervaren
+            </div>
+            <div>
+                <img id="img_card" src="./img/Abdominal-air-bike.jpg" alt="AbdominalAirBike" data-toggle="collapse" href="#AbdominalAirBike" aria-expanded="false" aria-controls="collapseExample"/>
+                <div class="text-center" data-toggle="collapse" href="#AbdominalAirBike" aria-expanded="false" aria-controls="collapseExample" style="height:35px;">
+                    <h5>Abdominal Air Bike</h5><br>
+                </div>
+                <div>
+                    <div class="collapse container" id="AbdominalAirBike">
+                        <br>
+                        -Ga plat liggen op de grond en plaats je handen achter je hoofd en draai je ellebogen naar voren, eigenlijk net zoals het begin positie van een crunch.<br />
+                        -Breng je knieën omhoog, zodat je bovenbenen verticaal omhoog staan en maak een hoek van 90 graden met  je knieën zodat je onderbenen parallel lopen met de vloer.<br />
+                        -Maak rustig en gecontrolleerd een fiets beweging, breng je recht been naar voren op het moment dat je je linker knie naar je toe brengt.<br />
+                        -Vervolgens breng je je rechter elleboog naar linker knie, door een crunch beweging naar de linkerkant.<br />
+                        -Ga terug naar de start positie, terwijl je in ademt en crunch daarna naar de andere kant terwijl je de fietsbeweging doormaakt waardoor je je linkerbeen strekt en je rechter knie weer naar je toe brengt.<br />
+                        -Herhaal dit met beide kanten.<br />
+                        <br /><br />
+                        <div class="text-center">
+                            <a class="btn btn-primary btn-block" href="index.php?content=scripts/pushup_script" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                voltooid
+                            </a><div style="height:8px;"></div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+    </div>
+    <div class="col-sm">
+        <div class="card">
+            <div id="licht" class="card-header text-center" data-toggle="collapse" href="#Crunches" aria-expanded="false" aria-controls="collapseExample">
+                beginner
+            </div>
+            <div>
+                <img id="img_card" src="./img/Crunch-uitvoering.jpg" alt="Crunches" data-toggle="collapse" href="#Crunches" aria-expanded="false" aria-controls="collapseExample"/>
+                <div class="text-center" data-toggle="collapse" href="#Crunches" aria-expanded="false" aria-controls="collapseExample" style="height:35px;">
+                    <h5>Crunches</h5><br>
+                </div>
+                <div>
+                    <div class="collapse container" id="Crunches">
+                        <br>
+                        Ga liggen op een fitnessmat<br />
+                        Breng de benen omhoog en houdt de knieën in een hoek van 90 graden.<br />
+                        Plaats de handen op de borst (of leg de handen onder het hoofd, trek niet aan de nek!)<br />
+                        Breng het hoofd, de schouders en de borst omhoog richting de knieën.<br />
+                        Kijk naar een vaste plek op het plafond en houdt de kin weg van de borst.<br />
+                        Breng het hoofd, de schouders en de borst weer naar beneden en herhaal voor het gewenste aantal herhalingen.<br />
+                        <br /><br />
+                        <div class="text-center">
+                            <a class="btn btn-primary btn-block" href="index.php?content=scripts/pushup_script" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                voltooid
+                            </a><div style="height:8px;"></div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+    </div>
+    <div class="col-sm">
+    <div class="card">
+            <div id="licht" class="card-header text-center" data-toggle="collapse" href="#CloseGripPushUp" aria-expanded="false" aria-controls="collapseExample">
+                beginner
+            </div>
+            <div>
+                <img id="img_card" src="./img/Close-Grip-Push-up-opdrukken.jpg" alt="CloseGripPushUp" data-toggle="collapse" href="#CloseGripPushUp" aria-expanded="false" aria-controls="collapseExample"/>
+                <div class="text-center" data-toggle="collapse" href="#CloseGripPushUp" aria-expanded="false" aria-controls="collapseExample" style="height:35px;">
+                    <h5>Close Grip Push Up</h5><br>
+                </div>
+                <div>
+                    <div class="collapse container" id="CloseGripPushUp">
+                        <br>
+                        -Neem een Push Up positie aan en plaats de handen dit keer dichterbij elkaar dan schouderbreedte.<br />
+                        -Laat jezelf zakken totdat je borst bijna de grond raakt.<br />
+                        -Duw jezelf nu weer omhoog.<br />
+                        -Herhaal dit voor het aantal gewenste repetities.<br />
+                        <br /><br />
+                        <div class="text-center">
+                            <a class="btn btn-primary btn-block" href="index.php?content=scripts/pushup_script" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                voltooid
+                            </a><div style="height:8px;"></div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+    </div>
+  </div>
+  <br>
+  
